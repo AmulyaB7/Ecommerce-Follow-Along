1a320a3b1a93b1abdceb26c172e2d163a2629ec4
Amulya B <amulyaboppana54@gmail.com>
2025-01-30 14:34:29 +0530

    commited

--- backend/server.js
+++ backend/server.js
@@ -17,11 +17,19 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
 // connect db
 connectDatabase();
 
-// create server
-// const server = app.listen
-// create server
+
 const server = app.listen(process.env.PORT, () => {
   console.log(
     `Server is running on http://localhost:${process.env.PORT}`
   );
+});
+
+// unhandled promise rejection(explain error handling when setting up server as you code)
+process.on("unhandledRejection", (err) => {
+  console.error(`Unhandled Rejection: ${err.message}`);
+  console.log("Shutting down the server due to unhandled promise rejection.");
+  
+  server.close(() => {
+    process.exit(1); // Exit with failure code
+  });
 });
