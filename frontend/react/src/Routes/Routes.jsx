2709a48fc7b1f4a6d140623a09f82d4dcbab72d5
Amulya B <amulyaboppana54@gmail.com>
2025-03-29 22:00:26 +0530

    commit

--- frontend/react/src/Routes/Routes.jsx
+++ frontend/react/src/Routes/Routes.jsx
@@ -2,7 +2,6 @@
 import Login from "../pages/Login.jsx";
 import Signup from "../pages/Signup.jsx";
 import Home from "../pages/Home.jsx";
-// import CreateProduct from "../pages/CreateProduct.jsx";
 import CreateProduct from "../pages/createProduct.jsx";
 import MyProducts from "../pages/MyProducts.jsx";
 import Cart from "../pages/cart.jsx";
